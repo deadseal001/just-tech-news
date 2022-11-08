a27573fc28a9509817a8effb067a94a2f8b986e6
deadseal001 <liwenbo4936@gmail.com>
2022-11-07 18:11:37 -0600

    express handlerbars

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require("express");
-const routes = require("./routes");
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
+const path = require("path");
+const exphbs = require("express-handlebars"); //handlebars
+const hbs = exphbs.create({}); //handlebars
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public"))); //set path so the files in public folder can be used.
 
 //turn on routes
 app.use(routes);
+app.engine("handlebars", hbs.engine); //handlebars
+app.set("view engine", "handlebars"); //handlebars
 
 //turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {

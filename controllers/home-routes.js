60d013ae1b294f85f98e211a1e6248e295ed329b
deadseal001 <liwenbo4936@gmail.com>
2022-11-08 21:07:30 -0600

    login user info

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -102,4 +102,48 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT (*) from vote where post.id=vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+      {
+        model: Comment,
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post foudn with this id" });
+        return;
+      }
+      //serialize the data
+      const post = dbPostData.get({ plain: true });
+      console.log(post);
+      //pass data to template
+      res.render("single-post", { post, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;

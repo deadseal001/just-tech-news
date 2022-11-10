3615c26cbc2be630bcf0173371baca124f331817
deadseal001 <liwenbo4936@gmail.com>
2022-11-09 21:22:36 -0600

    if

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -85,7 +85,7 @@ router.get("/", (req, res) => {
       // pass a single post object into the homepage template
       //   console.log(dbPostData[0]);
       const posts = dbPostData.map((post) => post.get({ plain: true }));
-      res.render("homepage", { posts }); //{posts}
+      res.render("homepage", { posts, loggedIn: req.session.loggedIn }); //{posts}
     })
     .catch((err) => {
       console.log(err);
